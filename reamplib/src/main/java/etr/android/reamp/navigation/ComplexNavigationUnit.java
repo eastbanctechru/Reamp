f40767417e464aeeb1a4653c1ce9f47ef0fcfea5
Artur Glyzin <forcelain@gmail.com>
2017-05-27 23:56:43 +0700

    navigation javadocs

--- reamplib/src/main/java/etr/android/reamp/navigation/ComplexNavigationUnit.java
+++ reamplib/src/main/java/etr/android/reamp/navigation/ComplexNavigationUnit.java
@@ -2,18 +2,39 @@ package etr.android.reamp.navigation;
 
 import android.content.Intent;
 
+/**
+ * Common navigation class which is designed to be used with {@link Navigation}
+ * <br/>
+ * Implement your navigation and data passing logic with the help of this class
+ * @param <T> Forward navigation data type
+ * @param <R> Backward (result) navigation data type
+ */
 public abstract class ComplexNavigationUnit<T, R> {
 
+    /**
+     * Implement your navigation logic here, such as starting an activity
+     * and passing arguments through an intent
+     */
     protected abstract void navigate(Navigation navigation);
 
+    /**
+     * Implement your logic of the retrieving a data you passed during the navigation,
+     * such as getting a data from an intent
+     */
     protected T getNavigationData(Navigation navigation) {
         return null;
     }
 
+    /**
+     * Implement your logic of the passing a result back to the requester
+     */
     protected void setNavigationResult(Navigation navigation) {
 
     }
 
+    /**
+     * Implement your logic of the retrieving a result
+     */
     protected R getNavigationResult(Navigation navigation, int requestCode, int resultCode, Intent data) {
         return null;
     }
--- reamplib/src/main/java/etr/android/reamp/navigation/Navigation.java
+++ reamplib/src/main/java/etr/android/reamp/navigation/Navigation.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 
 import etr.android.reamp.mvp.MvpView;
 
+/**
+ * Class which helps you to encapsulate your navigation logic
+ */
 public class Navigation {
 
     private final Activity activity;
@@ -20,26 +23,44 @@ public class Navigation {
         }
     }
 
+    /**
+     * Returns the activity which the navigation instance is connected to
+     */
     public Activity getActivity() {
         return activity;
     }
 
+    /**
+     * Finishes the current activity
+     */
     public void close() {
         activity.finish();
     }
 
+    /**
+     * Performs the navigation action which is provided by the passed navigationUnit
+     */
     public void open(ComplexNavigationUnit navigationUnit) {
         navigationUnit.navigate(this);
     }
 
+    /**
+     * Performs the data retrieving action which is provided by the passed navigationUnit
+     */
     public <T> T getData(ComplexNavigationUnit<T, ?> navigationUnit) {
         return navigationUnit.getNavigationData(this);
     }
 
+    /**
+     * Performs the result storing action which is provided by the passed navigationUnit
+     */
     public <R> void setResult(ComplexNavigationUnit<?, R> navigationUnit) {
         navigationUnit.setNavigationResult(this);
     }
 
+    /**
+     * Performs the result retrieving action which is provided by the passed navigationUnit
+     */
     public <R> R getResult(ComplexNavigationUnit<?, R> navigationUnit, int requestCode, int resultCode, Intent data) {
         return navigationUnit.getNavigationResult(this, requestCode, resultCode, data);
     }
